991bfac642fe0b2749b7c35e085826754bc22998
yschristian <yubahwesc@gmail.com>
2024-08-09 10:36:15 +0200

    tablet

--- src/screens/DetailsViewScreen.tsx
+++ src/screens/DetailsViewScreen.tsx
@@ -1,5 +1,7 @@
-import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, useWindowDimensions, Platform } from 'react-native';
 import { RouteProp } from '@react-navigation/native';
+import DeviceInfo from 'react-native-device-info';
 import { RootStackParamList } from '../types/root';
 
 type DetailsViewScreenRouteProp = RouteProp<RootStackParamList, 'DetailsView'>;
@@ -10,10 +12,15 @@ type Props = {
 
 const DetailsViewScreen = ({ route }: Props) => {
   const { item } = route.params;
+  const { width, height } = useWindowDimensions();
+  const isLandscape = width > height;
+  const isTablet = Platform.OS === 'ios' || Platform.OS === 'android';
+  // const isTablet = DeviceInfo.isTablet();  
+
 
   return (
-    <View style={styles.container}>
-      <View style={styles.card}>
+    <View style={[styles.container, isLandscape && styles.landscapeContainer]}>
+      <View style={[styles.card, isLandscape && isTablet && styles.landscapeCard]}>
         <Text style={styles.title}>{item.name}</Text>
         {item.data &&
           Object.entries(item.data).map(([key, value]) => (
@@ -33,6 +40,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
+  landscapeContainer: {
+    flexDirection: 'row',
+  },
   card: {
     backgroundColor: '#00264d',
     padding: 20,
@@ -46,6 +56,10 @@ const styles = StyleSheet.create({
     shadowRadius: 3.84,
     elevation: 5,
     opacity: 0.9,
+  },
+  landscapeCard: {
+    width: '80%',
+    paddingHorizontal: 30,
   },
   title: {
     fontSize: 24,
--- src/screens/ListViewScreen.tsx
+++ src/screens/ListViewScreen.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, TouchableOpacity, StyleSheet, ActivityIndicator } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet, ActivityIndicator, useWindowDimensions, Platform } from 'react-native';
 import axios from 'axios';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { useNavigation } from '@react-navigation/native';
 import { RootStackParamList } from '../types/root';
+import DeviceInfo from 'react-native-device-info'; 
 
 type Item = {
   id: string;
@@ -21,6 +22,12 @@ type ListViewScreenNavigationProp = StackNavigationProp<
 const ListViewScreen = () => {
   const [data, setData] = useState<Item[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const { width, height } = useWindowDimensions();
+  const isLandscape = width > height;
+  const isTablet = Platform.OS === 'ios' || Platform.OS === 'android';
+  // const isTablet = DeviceInfo.isTablet();
+  console.log('isTablet', isTablet);
+
   const navigation = useNavigation<ListViewScreenNavigationProp>();
 
   useEffect(() => {
@@ -41,7 +48,7 @@ const ListViewScreen = () => {
   
   const renderItem = ({ item }: { item: Item }) => (
     <TouchableOpacity onPress={() => navigation.navigate('DetailsView', { item })}>
-      <View style={styles.item}>
+      <View style={[styles.item, isLandscape && isTablet && styles.landscapeItem]}>
         <Text style={styles.text}>{item.name}</Text>
       </View>
     </TouchableOpacity>
@@ -49,30 +56,34 @@ const ListViewScreen = () => {
 
   return (
     <View style={styles.container}>
-    {isLoading ? (
-      <ActivityIndicator size="large" color="#0000ff" />
-    ) : (
-      <FlatList
-        data={data}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-      />
-    )}
-  </View>
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
+      )}
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor:"#00264d"
+    backgroundColor: "#00264d"
   },
   item: {
     padding: 20,
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
+  },
+  landscapeItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   text: {
     fontSize: 18,
